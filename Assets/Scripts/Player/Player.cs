cef4ad4e905c89ff3831fcef61c335c56d9cfe35
EddieT-TLee <adam832lee@gmail.com>
2026-06-17 19:37:25 -0500

    gumperton has animation now

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour
 
     private InputAction moveAction;
     private Rigidbody2D rb; // Players rigidbody used for movement
+    private Animator animator;
 
     void Awake()
     {
@@ -21,18 +22,35 @@ public class Player : MonoBehaviour
         InputActionMap playerMap = actionAsset.FindActionMap("Player");
         moveAction = playerMap.FindAction("Move");
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         if (PauseManager.IsPaused)
         {
+            UpdateAnimator(Vector2.zero); // Literally just to make it stop playing the walking animation
             rb.linearVelocity = Vector2.zero;
             return;
         }
 
         Vector2 input = moveAction.ReadValue<Vector2>();
         rb.linearVelocity = new Vector2(input.x * moveSpeed, input.y * moveSpeed);
+        UpdateAnimator(input);
+    }
+
+    private void UpdateAnimator(Vector2 input)
+    {
+        bool isMoving = input != Vector2.zero;
+        animator.SetBool("IsWalking", isMoving);
+        animator.SetFloat("InputX", input.x);
+        animator.SetFloat("InputY", input.y);
+
+        if (isMoving)
+        {
+            animator.SetFloat("LastInputX", input.x);
+            animator.SetFloat("LastInputY", input.y);
+        }
     }
 
     // Input action stuff
